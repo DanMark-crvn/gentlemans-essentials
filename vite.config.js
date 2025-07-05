3b0130acaea2b30cde26efd0bcfc27231b767f3d
DaMark-crvn <caravanadanmark@gmail.com>
2025-07-05 14:15:50 +0800

    changes on vite.config

--- vite.config.js
+++ vite.config.js
@@ -10,12 +10,12 @@ export default defineConfig({
         }),
         react(),
     ],
-    server: {
-        host: '192.168.18.30', //home WIFI Ip
-        // host: '192.168.18.139', //LAN HOME
-        // host: '192.168.1.15', //KEN
-        // host: '192.168.18.94', //WIN Ip
-        // host: '192.168.1.19', //home Kuya Ip
-        port: 5173,      // Default Vite port, change if necessary
-    }
+    // server: {
+    //     host: '', //WIFI Ip
+    //     // host: '', //LAN HOME
+    //     // host: '', //
+    //     // host: '', //Ip
+    //     // host: '', //Ip
+    //     port: 5173,      // Default Vite port, change if necessary
+    // }
 });
